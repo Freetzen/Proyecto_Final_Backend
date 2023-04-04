d7f00c2c2ecccea856dfbb8046c9eb1475b2965c
Freetzen <federuizgei@gmail.com>
2023-04-04 18:46:55 -0300

    Desafío Estrategia de autenticación

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -29,12 +29,12 @@ export const tryLogin = async (req, res) => {
                 res.redirect('/login')
             }
             if (!user) {
-                console.log(`TRYLOGIN> incorrect credentials`)
+                console.log(`TRYLOGIN> incorrecto`)
                 req.session.message = "Usuario o Contraseña incorrecta"
                 res.redirect('/login')
             }
 
-            console.log(`TRYLOGIN> authenticated`)
+            console.log(`TRYLOGIN> Autenticado`)
             req.session.login = true
             req.session.name = user.first_name
             req.session.role = user.role
@@ -48,44 +48,11 @@ export const tryLogin = async (req, res) => {
     })(req, res)
 }
 
-/* export const checkLogin = async (req, res) => {
-    try {
-        //
-        const { email, password } = req.body
-
-        if (email === "admin@coderhouse.com" && password === "admin12345") {
-            req.session.login = true
-            req.session.userFirst = "Admin Coder"
-            req.session.role = "admin"
-            console.log(`${email} logged in`)
-            res.redirect(200, '/products')
-        } else {
-            const user = await userManager.getUserByEmail(email)
-
-            if (user && validatePassword(password, user.password)) {
-                req.session.login = true
-                req.session.userFirst = user.first_name
-                req.session.role = user.role
-                console.log(`${email} logged in as ${user.role}`)
-                res.redirect(200, '/products')
-            } else {
-                res.status(401).json({
-                    message: "User or password incorrect"
-                })
-            }
-        }
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
-    }
-} */
-
 export const destroySession = (req, res) => {
     try {
         if (req.session.login) {
             req.session.destroy()
-            console.log(`Session closed`)
+            console.log(`Session cerrada`)
             res.status(200).redirect('/')
         }
     } catch (error) {
@@ -96,7 +63,7 @@ export const destroySession = (req, res) => {
 }
 
 export const requireAuth = (req, res, next) => {
-    console.log(`SESSIONCTRL> Is session active?: ${req.session.login}`);
+    console.log(`SESSIONCTRL> Sesión activa?: ${req.session.login}`);
     req.session.login ? next() : res.redirect("/login");
 
 }
